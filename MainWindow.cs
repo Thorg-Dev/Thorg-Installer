d77260601b5816f4ffa8718399ba24cb2f828285
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2022-05-26 12:52:54 +0200

    Moving elevation to Program.cs

--- MainWindow.cs
+++ MainWindow.cs
@@ -25,59 +25,11 @@ namespace Thorg_Installer
             PAGE_INSTALL = 4,
             PAGE_UNINSTALL = 5;
 
-        public static bool UserIsAdmin()
-        {
-            WindowsIdentity identity = WindowsIdentity.GetCurrent();
-            WindowsPrincipal principal = new WindowsPrincipal(identity);
-            return principal.IsInRole(WindowsBuiltInRole.Administrator);
-        }
-
-        public static bool RunElevated()
-        {
-            string args = string.Empty;
-            string[] argsArray = Environment.GetCommandLineArgs();
-            // First entry of the array must be ignored!
-            for (int i = 1; i < argsArray.Length; i++)
-            {
-                if (!string.IsNullOrEmpty(args))
-                    args += " ";
-                args += argsArray[i];
-            }
-            return RunElevated(System.Windows.Forms.Application.ExecutablePath, args);
-        }
-        /// <summary>
-        /// Restarts any application with admin rights.
-        /// </summary>
-        public static bool RunElevated(string fileName, string args)
-        {
-            ProcessStartInfo processInfo = new ProcessStartInfo();
-            processInfo.Verb = "runas";
-            processInfo.FileName = fileName;
-            processInfo.Arguments = args;
-            try
-            {
-                Process.Start(processInfo);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
             tabWizard.TabPages.Remove(tabUninstall);
             lbVersion.Text = "Loading...";
-
-            if (UserIsAdmin() == false)
-            {
-                if (RunElevated())
-                {
-                    Environment.Exit(0);
-                }
-            }
 
             _ = Task.Run(async () =>
             {
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
@@ -10,6 +11,46 @@ namespace Thorg_Installer
 {
     static class Program
     {
+        public static bool UserIsAdmin()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public static bool RunElevated()
+        {
+            string args = string.Empty;
+            string[] argsArray = Environment.GetCommandLineArgs();
+            // First entry of the array must be ignored!
+            for (int i = 1; i < argsArray.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(args))
+                    args += " ";
+                args += argsArray[i];
+            }
+            return RunElevated(System.Windows.Forms.Application.ExecutablePath, args);
+        }
+        /// <summary>
+        /// Restarts any application with admin rights.
+        /// </summary>
+        public static bool RunElevated(string fileName, string args)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.Verb = "runas";
+            processInfo.FileName = fileName;
+            processInfo.Arguments = args;
+            try
+            {
+                Process.Start(processInfo);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -18,6 +59,14 @@ namespace Thorg_Installer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            if (UserIsAdmin() == false)
+            {
+                if (RunElevated())
+                {
+                    Environment.Exit(0);
+                }
+            }
             Application.Run(new MainWindow());
         }
     }
